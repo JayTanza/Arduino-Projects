17efb8dac138b919bb37ff088d532d935d76821e
Jay Michael Tanza <85852693+JayTanza@users.noreply.github.com>
2023-02-16 11:38:58 +0800

    Add files via upload

---
+++ LED-OOP/LED-OOP.ino
@@ -0,0 +1,20 @@
+//MAIN CLASS
+#include "Common.h" 
+void setup() {
+// put your setup code here, to run once:
+Serial.begin(9600);
+initAll();
+}
+void loop() {
+// put your main code here, to run repeatedly:
+btn.onButton(onPushLED,offPushLED);
+//led.onButton();
+}
+void onPushLED()
+{
+led.onLED();
+}
+void offPushLED()
+{
+led.offLED();
+}
